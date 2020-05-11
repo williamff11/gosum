e526f30ed4f77d74575ab517c6f771d1c4287ff3
williamfreire <williamff11@gmail.com>
2020-05-11 17:20:12 -0300

    pull request

--- sum_test.go
+++ sum_test.go
@@ -5,6 +5,6 @@ import "testing"
 func TestSum(t *testing.T){
 	x := 5+5
     if x != 10 {
-		t.Error("Opa! 5 + 5 não é igual a 10, obtive", x)
+		t.Error("Opa!! 5 + 5 não é igual a 10, obtive", x)
 	   }
 	}
